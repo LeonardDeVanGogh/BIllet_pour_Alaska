589ce9128d675c183b3fe27ce6be4792541ebbea
Bryan Razyk <bryan.razyk@gmail.com>
2020-07-20 10:20:45 +0200

    prevent user to publish empty comment

--- controler/backEnd/addComment.php
+++ controler/backEnd/addComment.php
@@ -9,9 +9,9 @@ $dbh = $database->getConnection();
 $comment =  htmlspecialchars($_POST['addComment']);
 $articleId =  filter_var($_POST['articleId'], FILTER_VALIDATE_INT);
 
-
-$commentManager = new CommentManager($dbh);
-
-$commentManager->createComment($articleId,$comment);
+if($comment!=""){
+	$commentManager = new CommentManager($dbh);
+	$commentManager->createComment($articleId,$comment);
+}
 
 header("Location: index.php?page=billet&id_article=" . $articleId);
